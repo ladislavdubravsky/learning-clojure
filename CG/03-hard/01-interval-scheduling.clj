f7dee0ab43f7733e65a0f740aba76f916fe0cd78
Ladislav Dubravský <30551080+ldubravsky@users.noreply.github.com>
2017-09-02 20:27:36 +0200

    Create 01-interval-scheduling.clj

---
+++ CG/03-hard/01-interval-scheduling.clj
@@ -0,0 +1,19 @@
+(ns Solution
+    (:gen-class))
+
+(defn count-tasks [[[end start] & tasks] min-end cnt]
+    (cond (nil? end) cnt
+          (>= start min-end) (recur tasks end (inc cnt))
+          :else (recur tasks min-end cnt)))
+
+(defn -main [& args]
+    (let [n (read) _ (read-line)
+          tasks (repeatedly n read-line)
+          res (->> tasks
+                   (map #(clojure.string/split % #" "))
+                   (map #(map (fn [x] (Integer/parseInt x)) %))
+                   (map #(vector (+ (first %) (last %)) (first %)))
+                   sort
+                   (#(count-tasks % 0 0)))]
+
+        (println (if (= res 0) 1 res))))
